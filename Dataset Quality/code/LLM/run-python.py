39c3dd8734e2a8fcf74f0293934d05059805cf98
abcxyz <31880426+software-theorem@users.noreply.github.com>
2025-07-08 11:17:44 +0800

    Update run-python.py

--- Dataset Quality/code/LLM/run-python.py
+++ Dataset Quality/code/LLM/run-python.py
@@ -58,7 +58,7 @@ fewshot_example_10 = [
     }
 ]
 
-def generate_summaries_zero_shot(args, model, code, output_file, cnt=0):
+def generate_names_zero_shot(args, model, code, output_file, cnt=0):
     args.logger.info('zero-shot prompt...')
 
     with open(output_file, args.mode, encoding="utf-8") as f:
@@ -79,7 +79,7 @@ def generate_summaries_zero_shot(args, model, code, output_file, cnt=0):
             print('current idx:', idx)
 
 
-def generate_summaries_few_shot(args, model, code, output_file, cnt=0):
+def generate_names_few_shot(args, model, code, output_file, cnt=0):
     args.logger.info('few-shot prompt...')
     prompt = 'Here are ten examples of code and the corresponding method name.\n'
     for example in args.fewshot_example_10:
@@ -215,8 +215,8 @@ def main():
         write_ground_truth(gold,  dir + '/groundtruth.jsonl')
 
 
-    generate_summaries_zero_shot(args, model, codes, dir + 'zero_shot.jsonl', 0)
-    # generate_summaries_few_shot_10_example(args, model, code, dir + 'few_shot_10_example.csv', 0)
+    generate_names_zero_shot(args, model, codes, dir + 'zero_shot.jsonl', 0)
+    # generate_names_few_shot_10_example(args, model, code, dir + 'few_shot_10_example.csv', 0)
 
 if __name__ == '__main__':
     main()
